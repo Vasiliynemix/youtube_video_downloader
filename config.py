5ac321607e80ad2bc84d7da38173d92f7eac9c72
Vasiliynemix <vasyanemix@gmail.com>
2024-03-04 02:23:31 +0300

    init

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
-from dataclasses import dataclass, field
 from pathlib import Path
 
 from dotenv import load_dotenv
@@ -7,39 +6,31 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-@dataclass
 class GoogleConfig:
     auth_file_name: str = os.getenv("GOOGLE_AUTH_FILE_NAME")
     spreadsheet_id: str = os.getenv("SPREADSHEET_ID")
-    scopes: list[str] = field(default_factory=lambda: ["https://www.googleapis.com/auth/spreadsheets",
-                                                       "https://www.googleapis.com/auth/drive"])
+    scopes: list[str] = ["https://www.googleapis.com/auth/spreadsheets",
+                         "https://www.googleapis.com/auth/drive"]
 
 
-@dataclass
 class VideoConfig:
     video_dir_name: str = os.getenv("VIDEO_DIR_NAME")
     video_root_dir: str = os.getenv("VIDEO_ROOT_DIR")
 
 
-@dataclass
 class PathsConfig:
     root_path: str = str(Path(__file__).parent)
-    google: GoogleConfig = field(default_factory=GoogleConfig)
-    video: VideoConfig = field(default_factory=VideoConfig)
+    google: GoogleConfig = GoogleConfig
+    video: VideoConfig = VideoConfig
 
-    @property
-    def path_to_google_auth_file(self) -> str:
-        return os.path.join(self.root_path, self.google.auth_file_name)
+    path_to_google_auth_file = os.path.join(root_path, google.auth_file_name)
 
-    @property
-    def path_to_video_dir(self) -> str:
-        return os.path.join(self.video.video_root_dir, self.video.video_dir_name)
+    path_to_video_dir = os.path.join(video.video_root_dir, video.video_dir_name)
 
 
-@dataclass
 class Config:
-    google: GoogleConfig = field(default_factory=GoogleConfig)
-    paths: PathsConfig = field(default_factory=PathsConfig)
+    google: GoogleConfig = GoogleConfig
+    paths: PathsConfig = PathsConfig
 
 
 cfg = Config()
